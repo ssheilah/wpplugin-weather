c716c808c9e583a382f1ab79b57458d3cebc5f6b
ssheilah <sheilah.schneider@wsu.edu>
2017-01-09 12:40:48 -0800

    move js, correct html

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # WSUWP Weather
 
-[![Build Status](https://travis-ci.org/ssheilah/wpplugin-weather.svg?branch=master)](https://travis-ci.org/ssheilah/wpplugin-weather
+[![Build Status](https://travis-ci.org/ssheilah/wpplugin-weather.svg?branch=master)](https://travis-ci.org/ssheilah/wpplugin-weather)
 
 A WordPress plugin using the WSU framework; creates a shortcode to embed local weather.
+
+
--- includes/class-wsuwp-weather.php
+++ includes/class-wsuwp-weather.php
@@ -29,53 +29,32 @@ class WSUWP_Weather {
 	*/
 	public function setup_hooks() {
 
-		add_shortcode('weather', 'weather_shortcode');
+		add_shortcode( 'weather', 'weather_shortcode' );
 		add_action( 'wp_enqueue_scripts', array( $this, 'wsuf_weather_enqueue_scripts' ), 99 );
 
 		function weather_shortcode(){
     		
 			ob_start();
 			?> 
-
-<div class="container-fluid">
-  <div class="row photobox">
-    <div class="col-md-4 content-box">
-      <h1>What's it like outside?</h1>
-
-      <p>Location: <span id="location"></span></p>
-
-      <p>Latitude: <span id="lat"></span> || Longitude: <span id="lon"></span></p>
-
-
-      <p>Current conditions: <span id="conditions"></span></p>
-
-      <p>Temperature:</p>
-      <div>
-        <a id="k-button" class="btn btn-primary" data-toggle="collapse">
-       Kelvin</a>&nbsp;&nbsp;
-
-        <a id="c-button" class="btn btn-primary" data-toggle="collapse">
-    Celcius
-</a>&nbsp;&nbsp;
-        <a id="f-button" class="btn btn-primary" data-toggle="collapse">
-    Fahrenheit
-</a>&nbsp;&nbsp;<span id="temperatureK"></span><span id="temperatureF"></span><span id="temperatureC"></span>
-      </div>
-
-
-      <p>&nbsp;</p>
-      <p>Forecast: <span id="foreca"></span></p>
-      <p class="small">Weather via <a href="http://openweathermap.org/" target="_blank">openweathermap.org</a> || Photos courtesy of <a href="https://unsplash.com/"
-          target="_blank">Unsplash</a></p>
-    </div>
-    <div class="col-md-8">
-
-
-    </div>
-
-  </div>
-</div>
-</div>
+				<div class="container-fluid">
+					<div class="row photobox">
+						<div class="col-md-4 content-box">
+							<h1>What's it like outside?</h1>
+							<p>Location: <span id="location"></span></p>
+							<p>Latitude: <span id="lat"></span> || Longitude: <span id="lon"></span></p>
+							<p>Current conditions: <span id="conditions"></span></p>
+							<p>Temperature:</p>
+							<div>
+								<a id="k-button" class="btn btn-primary" data-toggle="collapse">Kelvin</a>&nbsp;&nbsp;<a id="c-button" class="btn btn-primary" data-toggle="collapse">Celcius</a>&nbsp;&nbsp;<a id="f-button" class="btn btn-primary" data-toggle="collapse">Fahrenheit</a>&nbsp;&nbsp;<span id="temperatureK"></span><span id="temperatureF"></span><span id="temperatureC"></span>
+							</div>
+							<p>&nbsp;</p>
+							<p>Forecast: <span id="foreca"></span></p>
+							<p class="small">Weather via <a href="http://openweathermap.org/" target="_blank">openweathermap.org</a> || Photos courtesy of <a href="https://unsplash.com/"	target="_blank">Unsplash</a></p>
+						</div> 
+						<div class="col-md-8">
+						</div>
+					</div>
+				</div>
 			<?php
 			return ob_get_clean();
 		}
@@ -86,13 +65,13 @@ class WSUWP_Weather {
 	/* Enqueues JavaScript and CSS files
 	*/
 	function wsuf_weather_enqueue_scripts() {
-		wp_enqueue_script( 'wsuf_weather', plugins_url( '/weather-js.js', __FILE__ ), array( 'jquery' ), '1.0', true );
-		wp_enqueue_script( 'bootstrap_toggle', 'https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js', array('jquery'), true);
+		wp_enqueue_script( 'wsuf_weather', plugins_url( '../js/weather-js.js', __FILE__ ), array( 'jquery' ), '1.0', true );
+		wp_enqueue_script( 'bootstrap_toggle', 'https://gitcdn.github.io/bootstrap-toggle/2.2.2/js/bootstrap-toggle.min.js', array( 'jquery' ), true);
 		wp_enqueue_script( 'bootstrap_twitter', 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js', true );
 
 		wp_enqueue_style( 'wsuf_style', plugins_url( 'css/style.css', dirname( __FILE__ ) ), array( 'spine-theme' ), null );
 		wp_enqueue_style( 'bootstrap_min', 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css' );
-		wp_enqueue_style( 'bootstrap_tog2', 'https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css');
+		wp_enqueue_style( 'bootstrap_tog2', 'https://gitcdn.github.io/bootstrap-toggle/2.2.2/css/bootstrap-toggle.min.css' );
 	}
 }
 
